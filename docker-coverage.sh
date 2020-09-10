c1e1658208f6f13fda266c2051397fa177babf75
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-09-10 12:43:02 +0200

    Added test requirements DAT-4127

--- docker-coverage.sh
+++ docker-coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 # - this entrypoint expect one argument: a directory which contains the root of a Python application
-# - it expects this directory to contain a requirements-test.txt file
+# - it expects this directory to contain a requirements.txt file
 # - it will copy this directory to a tmpdir
-# - install the test dependencies
+# - install the (test) dependencies
 # - and run the unittests using coverage from there, in a virtual env
 #
 set -eo pipefail
@@ -12,24 +12,28 @@ if [ "$BRANCH_NAME" == "master" ]; then
     exit 0
 fi
 if [ "$1" == "" ]; then
-    echo "This container expects one argument: a Python application directory "
+    echo "This container expects one argument: a Python application directory. Failing..."
     exit 1
 fi
-if [ -f "$1/requirements-test.txt" ]; then
+if [ -f "$1/requirements.txt" ]; then
     MYTEMP=$(mktemp -d -u)
     echo "Copying $1 to ${MYTEMP}"
-    cp -ap $1 ${MYTEMP}
-    cd ${MYTEMP}
+    cp -ap "$1" "${MYTEMP}"
+    echo "Copying test requirements to ${MYTEMP}"
+    cp requirements-test.txt "${MYTEMP}"
+    cd "${MYTEMP}"
     echo "Creating virtualenv"
     virtualenv -p python3.7 venv
+    # shellcheck disable=SC1091
     source venv/bin/activate
-    echo "Running pip install -r requirements-test.txt"
-    pip install -r requirements-test.txt
+    echo "Installing requirements"
+    pip install -r requirements.txt
+    pip install -r /requirements-test.txt
     export GOOGLE_CLOUD_PROJECT="${PROJECT_ID}"
     echo "Running coverage run -m unittest on ${PROJECT_ID}"
     coverage run -m unittest
     coverage report -m
 else
-    echo "Skipping unittesting due to missing $1/requirements-test.txt"
-    exit 0
+    echo "$1/requirements.txt was not found. Failing..."
+    exit 1
 fi
