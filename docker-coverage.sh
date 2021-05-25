008707767b3f00da045d680bb4aebf2798bed8f6
SpringPie <bente.vandepol@gmail.com>
2021-05-25 16:04:37 +0200

    Testing API key env DAT-6601

--- docker-coverage.sh
+++ docker-coverage.sh
@@ -30,8 +30,8 @@ if [ -f "$1/requirements.txt" ]; then
     pip install -r requirements.txt
     pip install -r /requirements-test.txt
     export GOOGLE_CLOUD_PROJECT="${PROJECT_ID}"
-    export API_KEY="${TEST_API_KEY}"
-    echo "TEST_API_KEY: ${TEST_API_KEY}"
+    export API_KEY="$TEST_API_KEY"
+    echo "TEST_API_KEY: $TEST_API_KEY"
     echo "API_KEY: ${API_KEY}"
     echo "Running coverage run -m unittest on ${PROJECT_ID}"
     coverage run -m unittest
